15fa4ae62656664ef85b3116296cb410d89aa52d
Dolganov Kirill <dolganovk.exceedteam@gmail.com>
2022-04-18 15:46:43 +0300

    Added token

--- src/modules/controllers/user.controller.js
+++ src/modules/controllers/user.controller.js
@@ -1,6 +1,14 @@
 const db = require('../../db/database');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
 const salt = bcrypt.genSaltSync(15);
+
+const generateAccessToken = (login) => {
+  return jwt.sign(login, process.env.TOKEN);
+}
 
 const hash = (password) => {
   const hashPass = bcrypt.hashSync(password, salt);
@@ -11,8 +19,9 @@ module.exports.createNewUser = async (req, res) => {
   try {
     const { login, password } = req.body;
     if (!(login && password)) return res.status(422).send('Error! Params not found!');
-    const newUser = await db.query(`INSERT INTO users (login, password) values ($1, $2) RETURNING *`, [login, hash(password)]);
-    res.send(newUser.rows[0]);
+    await db.query(`INSERT INTO users (login, password) values ($1, $2) RETURNING *`, [login, hash(password)]);
+    const token = generateAccessToken(login);
+    res.send(token);
   } catch (error) {
     return res.status(422).send({ error, message: 'Error! Params not correct!' });
   }
