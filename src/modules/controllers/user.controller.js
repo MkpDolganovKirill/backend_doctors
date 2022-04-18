6c58f96bc677197c2b7e221f2feccb38a0164025
Dolganov Kirill <dolganovk.exceedteam@gmail.com>
2022-04-18 12:11:22 +0300

    Fixed hash password with create.

--- src/modules/controllers/user.controller.js
+++ src/modules/controllers/user.controller.js
@@ -1,10 +1,16 @@
 const db = require('../../db/database');
+const crypto = require('crypto');
+
+const hash = (password) => {
+  const hashPass = crypto.createHash('sha256').update(password).digest('base64');
+  return hashPass;
+} 
 
 module.exports.createNewUser = async (req, res) => {
   try {
     const { login, password } = req.body;
     if (!(login && password)) return res.status(422).send('Error! Params not found!');
-    const newUser = await db.query(`INSERT INTO users (login, password) values ($1, $2) RETURNING *`, [login, password]);
+    const newUser = await db.query(`INSERT INTO users (login, password) values ($1, $2) RETURNING *`, [login, hash(password)]);
     res.send(newUser.rows[0]);
   } catch (error) {
     return res.status(422).send({ error, message: 'Error! Params not correct!' });
