789d5c66818504c00550deecb625e9951a67f3a9
Dolganov Kirill <dolganovk.exceedteam@gmail.com>
2022-04-18 16:38:46 +0300

    Added authorization request.

--- src/modules/controllers/user.controller.js
+++ src/modules/controllers/user.controller.js
@@ -1,5 +1,5 @@
 const db = require('../../db/database');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -21,7 +21,24 @@ module.exports.createNewUser = async (req, res) => {
     if (!(login && password)) return res.status(422).send('Error! Params not found!');
     await db.query(`INSERT INTO users (login, password) values ($1, $2) RETURNING *`, [login, hash(password)]);
     const token = generateAccessToken(login);
-    res.send(token);
+    res.send({ token: token, username: login });
+  } catch (error) {
+    return res.status(422).send({ error, message: 'Error! Params not correct!' });
+  }
+}
+
+module.exports.authorizationUser = async (req, res) => {
+  try {
+    const { login, password } = req.body;
+    if (!(login && password)) return res.status(422).send('Error! Params not found!');
+    const result = await db.query(`SELECT * FROM users WHERE login='${login}'`);
+    const user = result.rows[0];
+    if (bcrypt.compareSync(password, user.password)) {
+      const token = generateAccessToken(login);
+      res.send({token: token, username: login});
+    } else {
+      res.status(404).send('Invalid username or password!');
+    }
   } catch (error) {
     return res.status(422).send({ error, message: 'Error! Params not correct!' });
   }
