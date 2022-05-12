8bb744181051661861478f924bfa44eb16387eda
Dolganov Kirill <dolganovk.exceedteam@gmail.com>
2022-05-12 10:56:23 +0300

    add delete refresh token request

--- src/modules/routes/user.routes.js
+++ src/modules/routes/user.routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const {
   createNewUser,
-  authorizationUser
+  authorizationUser,
+  deleteRefreshToken
 } = require('../controllers/user.controller');
 
 router.post('/createNewUser', createNewUser);
 router.post('/authorizationUser', authorizationUser);
+router.delete('/deleteRefreshToken', deleteRefreshToken);
 
 module.exports = router;
